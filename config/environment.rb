04ca07ac926829eb23faa5959409ca1386d29f52
Zach <yonash402@gmail.com>
2013-02-15 21:03:53 -0500

    modified:   config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,18 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Dt10::Application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base..smtp_settings = {
+
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "dormtown.com",
+    :user_name => "contact@dormtown.com",
+    :password => "mynameisurl99",
+    :authentication => "plain",
+    :enable_starttls_auto => true }
+
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+
+
