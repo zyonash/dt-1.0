f6b3223e596777847e8376ba11cda0fc65d8661b
Zach <yonash402@gmail.com>
2013-02-17 13:34:07 -0500

    modified:   app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :confirmable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # :email_regexp => /.+@pitt.edu$/i
+         :recoverable, :rememberable, :trackable, :validatable, :email_regexp => /.+@pitt.edu$/i
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
